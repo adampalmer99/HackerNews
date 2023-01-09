d4e5de4ce17c6b33a638ca986821dc7ac52c1baa
Adam Palmer <acpalmer1011@gmail.com>
2023-01-09 10:22:05 +0000

    Push to github

---
+++ H4X0R News/Views/DetailView.swift
@@ -0,0 +1,26 @@
+//
+//  DetailView.swift
+//  H4X0R News
+//
+//  Created by Adam Palmer on 08/09/2021.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+    
+    
+ 
